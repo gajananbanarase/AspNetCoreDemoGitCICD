ef5b12c1f17b8ef1efdcbfde220e6ff773aa13c5
gajananbanarase <gbanarase450@gmail.com>
2024-02-28 20:04:37 +0530

    Inital commit

---
+++ AspNetCoreDemoGitCICD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspNetCoreDemoGitCICD.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
